360647a7da41052630887b69b79e35e11ba9ed8e
adman9 <98952525+adman9@users.noreply.github.com>
2025-05-05 20:17:04 +0200

    update

--- bank-data-pipeline/bnk-data-gcs-bq/utils/load_bigquery.py
+++ bank-data-pipeline/bnk-data-gcs-bq/utils/load_bigquery.py
@@ -1,15 +1,15 @@
 from google.cloud import bigquery
+import pandas as pd
 import logging
 
 
-def load_data_bigquery(project_id, dataset_id,  data):
+def load_data_bigquery(project_id, dataset_id,  data: pd.DataFrame):
     try:
         client = bigquery.Client()
 
         table_id = f"{project_id}.{dataset_id}.bank_trans_raw_temp_table"
         job_config = bigquery.LoadJobConfig(
             write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
-            source_format=bigquery.SourceFormat.CSV,
             autodetect=True,
             )
         load_job = client.load_table_from_dataframe(data, table_id, job_config=job_config)   # API request
