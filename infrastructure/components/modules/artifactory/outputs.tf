5f022d9537561ec619129279cdff0440e4144c15
adman9 <98952525+adman9@users.noreply.github.com>
2025-05-05 09:50:31 +0200

    Update outputs.tf

--- infrastructure/components/modules/artifactory/outputs.tf
+++ infrastructure/components/modules/artifactory/outputs.tf
@@ -1,4 +1,4 @@
 output "docker_repository" {
   description = "The full path to the Docker repository."
-  value       = "${var.region}-docker.pkg.dev/${var.project_id}/${var.docker_repository_id}/${var.image_name}:latest"
+  value       = "projects/${var.project_id}/locations/${var.region}/repositories/${var.docker_repository_id}/${var.image_name}:latest"
 }
