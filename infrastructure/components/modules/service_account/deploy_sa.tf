c94ada676725c73083f9a6c9ec67cef6d24bf8df
adman9 <98952525+adman9@users.noreply.github.com>
2025-05-05 14:45:57 +0200

    Update deploy_sa.tf

--- infrastructure/components/modules/service_account/deploy_sa.tf
+++ infrastructure/components/modules/service_account/deploy_sa.tf
@@ -33,6 +33,13 @@ resource "google_project_iam_member" "logs_writer" {
   member  = "serviceAccount:${google_service_account.cloud_function_sa.email}"
 }
 
+resource "google_project_iam_member" "storage_admin" {
+  project = var.project_id
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:${google_service_account.cloud_function_sa.email}"
+}
+
+
 resource "google_service_account_iam_binding" "cloud_function_sa_use" {
   service_account_id = google_service_account.cloud_function_sa.name
   role    = "roles/iam.serviceAccountUser"
