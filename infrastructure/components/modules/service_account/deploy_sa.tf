e2f03fe26283e7441246cd1bb368ef9e422ecb7d
adman9 <98952525+adman9@users.noreply.github.com>
2025-05-05 14:30:31 +0200

    Update deploy_sa.tf

--- infrastructure/components/modules/service_account/deploy_sa.tf
+++ infrastructure/components/modules/service_account/deploy_sa.tf
@@ -43,4 +43,12 @@ resource "google_service_account_iam_binding" "cloud_function_sa_use" {
   
 }
 
+resource "google_service_account_iam_binding" "cloud_function_sa_use" {
+  service_account_id = "${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+  role    = "roles/iam.serviceAccountUser"
+  members  = [
+    "serviceAccount:sa-cicd@favorable-kiln-458413-r9.iam.gserviceaccount.com",
+  ]
+  
+}
 
