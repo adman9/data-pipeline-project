5b5d598c5e0149b79c19f888c7a790d62b85220c
adman9 <98952525+adman9@users.noreply.github.com>
2025-05-05 03:15:25 +0200

    Update deploy_cf.tf

--- infrastructure/components/modules/cloud_functions/deploy_cf.tf
+++ infrastructure/components/modules/cloud_functions/deploy_cf.tf
@@ -7,32 +7,42 @@ terraform {
   }
 }
 
-resource "google_cloudfunctions2_function" "my_cloud_function" { 
-  name                  = var.function_name
-  runtime               = var.runtime
-  service_account_email = var.service_account_email
-  event_trigger {
-    event_type = "google.cloud.storage.object.v1.finalized"
-    bucket = var.trigger_bucket_name
-  }
+resource "google_cloudfunctions2_function" "my_cloud_function" {
+  name = var.function_name
+  location = var.region
+  description = "cloud Function to load the files from storage to big query"
+
   build_config {
+    runtime     = var.runtime
+    entry_point = "data_loading" 
+    environment_variables = {
+      "PROJECT_ID" = var.project_id
+      "REGION" = var.region
+      "DATASET" = "BNK_DATA"
+      "ARCHIVE_BUCKET" = var.archival_bucket_name
+      "SPLITTER" = ","
+    }
     docker_repository = var.docker_repository
   }
+
   service_config {
-    max_instance_count = var.max_instance_count
-    available_memory = var.available_memory
-    timeout_seconds = var.timeout_seconds
+    max_instance_count  = var.max_instance_count
+    available_memory    = var.available_memory
+    timeout_seconds     = var.timeout_seconds
+    environment_variables = {
+        SERVICE_CONFIG_TEST = "config_test"
+    }
+    ingress_settings = "ALLOW_ALL"
+    service_account_email = var.service_account_email
   }
-  environment_variables = {
-    "PROJECT_ID" = var.project_id
-    "REGION" = var.region
-    "DATASET" = "BNK_DATA"
-    "ARCHIVE_BUCKET" = var.archival_bucket_name
-    "SPLITTER" = ","
+
+  event_trigger {
+    event_type = "google.cloud.storage.object.v1.finalized"
+    event_filters {
+      attribute = "bucket"
+      value = var.trigger_bucket_name
+    }
   }
-  depends_on = [
-    google_artifact_registry_repository.my_docker_repository,
-  ]
 }
 
 resource "google_cloudfunctions2_function_iam_member" "member" {
